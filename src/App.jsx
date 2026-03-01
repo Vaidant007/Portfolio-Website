e077ce578fb2f28a7ca443b0e7051b03acc92f37
Vaidant007 <vaidantshrivastava7@gmail.com>
2026-03-01 13:55:58 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from "./components/skills/Skills";
-
+import { Routes, Route } from 'react-router-dom';
 import Project from './components/projects/Project';
 import Contact from './components/contact/Contact';
+
 const App = () => {
   return (
     <>
     <Header/>
     <main className="main">
-      <Home/>
-      <About/>
-     <Skills/>
-     <Project/>
-     <Contact/>
+      <Routes>
+      <Route path="/" element ={<Home/>}/>
+      <Route path="/about" element ={<About/>}/>
+      <Route path="/skills" element ={<Skills/>}/>
+      <Route path="/project" element ={<Project/>}/>
+      <Route path="/contact" element ={<Contact/>}/>
+      </Routes>
+      
     </main>
     </>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,61 +1,53 @@
-import React, { useState } from 'react'
-import "./header.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
 const Header = () => {
-    const[Toggle,showMenu]=useState(false);
+  const [Toggle, showMenu] = useState(false);
+
   return (
     <header className="header">
-        <nav className="nav container">
-            <a href="index.html" className="nav__logo">Vaidant</a>
-            <div className={Toggle ?"nav__menu show-menu":"nav__menu"}>
-                <ul className="nav__list grid">
-                    <li className="nav__item">
-                        <a href="#home" className="nav__link active-link">
-                            <i className="uil uil-estate nav__icon"></i>
-                          Home
-                        </a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="#about" className="nav__link">
-                        <i className="uil uil-user nav__icon"></i>
-                            About
-                        </a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="#skills" className="nav__link">
-                        <i className="uil uil-user nav__icon"></i>
-                            Skills
-                        </a>
-                    </li>
-                    <li className="nav__item">
-                        <a href="#project" className="nav__link">
-                        <i className="uil uil-scenery nav__icon"></i>
-                            Projects
-                        </a>
-                    </li>
-                    
-                    <li className="nav__item">
-                        <a href="#contact" className="nav__link">
-                        <i className="uil uil-message nav__icon"></i>
-                            Contact
-                        </a>
-                    </li>
-                    
-                    
-                     
-                </ul>
-             
-                
+      <nav className="nav container">
+        <Link to="/" className="nav__logo">Vaidant</Link>
 
-                <i class="uil uil-times nav__close" onClick={()=>showMenu(false)}></i>
-            </div>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <Link to="/" className="nav__link" onClick={() => showMenu(false)}>
+                <i className="uil uil-estate nav__icon"></i> Home
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/about" className="nav__link" onClick={() => showMenu(false)}>
+                <i className="uil uil-user nav__icon"></i> About
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/skills" className="nav__link" onClick={() => showMenu(false)}>
+                <i className="uil uil-briefcase nav__icon"></i> Skills
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/project" className="nav__link" onClick={() => showMenu(false)}>
+                <i className="uil uil-scenery nav__icon"></i> Projects
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/contact" className="nav__link" onClick={() => showMenu(false)}>
+                <i className="uil uil-message nav__icon"></i> Contact
+              </Link>
+            </li>
+          </ul>
 
-            <div className="nav__toggle" onClick={()=>showMenu(!Toggle)}>
-            <i class="uil uil-apps"></i>
-            </div>
-        </nav>
+          <i className="uil uil-times nav__close" onClick={() => showMenu(false)}></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
